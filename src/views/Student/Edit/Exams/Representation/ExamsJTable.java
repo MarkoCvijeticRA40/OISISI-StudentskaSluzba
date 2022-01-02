57d9e8d8fe8f2e9fa72bbc80e4874c9330e1ac09
markor33 <rapic.ra42.2019@uns.ac.rs>
2022-01-02 01:20:17 +0100

    Implemented #representation_of_npassed_exams

---
+++ src/views/Student/Edit/Exams/Representation/ExamsJTable.java
@@ -0,0 +1,45 @@
+package views.Student.Edit.Exams.Representation;
+
+import java.awt.Color;
+import java.awt.Component;
+
+import javax.swing.BorderFactory;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.TableCellRenderer;
+
+public class ExamsJTable extends JTable {
+	
+	private static final long serialVersionUID = -596692514521391681L;
+
+	public ExamsJTable() {
+		this.setRowSelectionAllowed(true);
+		this.setColumnSelectionAllowed(false);
+		this.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		this.getTableHeader().setBackground(Color.LIGHT_GRAY);
+		this.getTableHeader().setBorder(BorderFactory.createMatteBorder(1, 1, 0, 1, Color.BLACK));
+		this.setBorder(BorderFactory.createMatteBorder(0, 1, 1, 1, Color.BLACK));
+		this.setModel(new AbstractTableModelExams());
+	}
+	
+	@Override
+	public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
+		Component c = super.prepareRenderer(renderer, row, column);
+		if (isRowSelected(row)) {
+			c.setBackground(Color.MAGENTA);
+		}
+		else {
+			if (row % 2 == 0)
+				c.setBackground(Color.WHITE);
+			else
+				c.setBackground(Color.LIGHT_GRAY);
+		}
+		return c;
+	}
+	
+	public void updateView() {
+		AbstractTableModelExams model = (AbstractTableModelExams) this.getModel();
+		model.fireTableDataChanged();
+		this.setRowSelectionInterval(0, 0);
+	}
+}
