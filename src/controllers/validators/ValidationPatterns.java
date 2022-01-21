1133b988fa11ffe008e4409a27be74f1ad693daf
markor33 <rapic.ra42.2019@uns.ac.rs>
2022-01-21 01:24:08 +0100

    Validation patterns update

--- src/controllers/validators/ValidationPatterns.java
+++ src/controllers/validators/ValidationPatterns.java
@@ -9,12 +9,12 @@ public class ValidationPatterns {
 	public static final String onlyAlphabets = "[a-zA-Z]+";
 	public static final String onlyAlphabetsWithWhiteSpace = "\\S[a-zA-Z\s]+";
 	public static final String email = "[a-z]+[a-z0-9.]*[a-z0-9]+@[a-z]+[a-z.]*[a-z]+\\.[a-z]+";
-	public static final String phoneNumber = "0[1-9]{2}[0-9]+";
+	public static final String phoneNumber = "0[1-9]{2}/[0-9-]+";
 	public static final String idCardNumber = "[0-9]{9}";
 	public static final String number = "[1-9][0-9]*";
-	public static final String indexNumber = "[A-Z]+[0-9]+/[0-9]+";
+	public static final String indexNumber = "[A-Z]+ [0-9]+/[0-9]+";
 	public static final String enrollmentYear = "[1-9][0-9]{3}";
 	// smerBR/BR
 	//DATE
-	public static final SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+	public static final SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy.");
 }
